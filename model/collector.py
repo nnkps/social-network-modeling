19ed4ec5e934fe5b6bdea3b1617aa7b5fb8cfb4c
Ania Kupś <aniakups@gmail.com>
2016-06-17 09:10:50 +0200

    Removed unnecessary file

--- model/collector.py
+++
@@ -1,4 +0,0 @@
-
-def compute_avg_number_of_comments(model):
-	comments_numbers = [len(agent.comments) for agent in model.schedule.agents]
-	return sum(comments_numbers) / len(model.schedule.agents)
